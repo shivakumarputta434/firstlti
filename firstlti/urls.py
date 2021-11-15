4c2b64fd7fc077c40363ee8011b4ef86d51754a5
shivakumarputta434 <shivakumar.putta434@gmail.com>
2021-11-15 10:35:11 +0530

    new code modified in views file

--- firstlti/urls.py
+++ firstlti/urls.py
@@ -19,5 +19,6 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home',views.home,name="home")
+    path('home',views.home,name="home"),
+    path('add',views.add,name="add")
 ]
--- firstlti/views.py
+++ firstlti/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     return HttpResponse("our new project updated")
+
+def add(request):
+    num1=10
+    num2=20
+    num3=num1+num2
+    return HttpResponse("{0} and {1}Total is: {2}".format(num1,num2,num3))
